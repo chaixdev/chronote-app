8cd101ae8c250202295db3ce9a9a0e6873cce69a
chaitanya <bhagwat.chaitanya@gmail.com>
2020-08-05 03:30:13 +0200

    fix dao on main thread issue

--- app/src/main/java/be/chaidev/chronote/data/repository/TopicsRepository.kt
+++ app/src/main/java/be/chaidev/chronote/data/repository/TopicsRepository.kt
@@ -1,17 +1,17 @@
 package be.chaidev.chronote.data.repository
 
+import android.util.Log
 import be.chaidev.chronote.data.cache.CacheMapper
 import be.chaidev.chronote.data.cache.dao.TopicDao
 import be.chaidev.chronote.data.model.Topic
 import be.chaidev.chronote.data.network.retrofit.NetworkMapper
 import be.chaidev.chronote.data.network.retrofit.StreamarksApi
 import be.chaidev.chronote.util.DataState
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import javax.inject.Inject
 
-class TopicsRepository @Inject constructor(
+
+class TopicsRepository constructor(
     private val topicDao: TopicDao,
     private val streamarksApi: StreamarksApi,
     private val cacheMapper: CacheMapper,
@@ -20,12 +20,13 @@ class TopicsRepository @Inject constructor(
 
     suspend fun getTopics(): Flow<DataState<List<Topic>>> = flow{
         emit(DataState.Loading)
-        // todo: remove delay
-        delay(1000)
         try{
+            Log.d("network", "calling streamarksApi.getTopics()")
             val networkEntities = streamarksApi.getTopics()
             val topics = networkMapper.mapFromEntitiesList(networkEntities)
-            topicDao.insertAll(*cacheMapper.mapToEntitiesList(topics).toTypedArray())
+            for(topic in topics){
+                topicDao.insert(cacheMapper.mapToEntity(topic))
+            }
 
             val cachedTopics = topicDao.getAll()
             emit(DataState.Success(cacheMapper.mapFromEntitiesList(cachedTopics)))
