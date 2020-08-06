02d48e57765f434ca6eeec0480f702b1cfa8e26a
chaitanya <bhagwat.chaitanya@gmail.com>
2020-08-06 22:33:43 +0200

    extract abstract TopicFragment

---
+++ app/src/main/java/be/chaidev/chronote/ui/mvi/UICommunicationListener.kt
@@ -0,0 +1,27 @@
+package be.chaidev.chronote.ui.mvi
+
+import be.chaidev.chronote.ui.AreYouSureCallback
+
+interface UICommunicationListener {
+
+    fun onUIMessageReceived(uiMessage: UIMessage)
+}
+
+
+data class UIMessage(
+    val message: String,
+    val uiMessageType: UIMessageType
+)
+
+sealed class UIMessageType{
+
+    class Toast: UIMessageType()
+
+    class Dialog: UIMessageType()
+
+    class AreYouSureDialog(
+        val callback: AreYouSureCallback
+    ): UIMessageType()
+
+    class None: UIMessageType()
+}
